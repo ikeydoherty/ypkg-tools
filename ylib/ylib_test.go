75db0a1dab314dd8021d1c28d3712add34591b24
Ikey Doherty <ikey@solus-project.com>
2016-08-06 18:35:35 +0100

    Simplify the tests a lot
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- ylib/ylib_test.go
+++ ylib/ylib_test.go
@@ -5,52 +5,28 @@ import (
 )
 
 func TestExamineURI(t *testing.T) {
-	// url := args[1]
-	if info := ExamineURI("https://github.com/solus-project/linux-steam-integration/releases/download/v0.2/linux-steam-integration-0.2.tar.xz"); info != nil {
-		if info.PkgName != "linux-steam-integration" {
-			t.FailNow()
-		}
-		if info.Version != "0.2" {
-			t.FailNow()
-		}
-	} else {
-		t.FailNow()
-	}
+
+	uri_tests := make(map[string]SourceInfo)
+
+	uri_tests["https://github.com/solus-project/linux-steam-integration/releases/download/v0.2/linux-steam-integration-0.2.tar.xz"] = SourceInfo{PkgName: "linux-steam-integration", Version: "0.2"}
 
 	// github v URL
-	if info := ExamineURI("https://github.com/solus-project/linux-steam-integration/archive/v0.2.tar.gz"); info != nil {
-		if info.PkgName != "linux-steam-integration" {
-			t.FailNow()
-		}
-		if info.Version != "0.2" {
-			t.FailNow()
-		}
-	} else {
-		t.FailNow()
-	}
+	uri_tests["https://github.com/solus-project/linux-steam-integration/archive/v0.2.tar.gz"] = SourceInfo{PkgName: "linux-steam-integration", Version: "0.2"}
 
 	// pypi url with odd name package
-	if info := ExamineURI("https://pypi.python.org/packages/fc/f1/7530ac8594453fc850e53580256f3152a8d8f2bb351bc3d0df8d7b53dbde/ruamel.yaml-0.11.11.tar.gz"); info != nil {
-		if info.PkgName != "ruamel.yaml" {
-			t.FailNow()
-		}
-		if info.Version != "0.11.11" {
-			t.FailNow()
-		}
-	} else {
-		t.FailNow()
-	}
+	uri_tests["https://pypi.python.org/packages/fc/f1/7530ac8594453fc850e53580256f3152a8d8f2bb351bc3d0df8d7b53dbde/ruamel.yaml-0.11.11.tar.gz"] = SourceInfo{PkgName: "ruamel.yaml", Version: "0.11.11"}
 
 	// Very odd sourceforge URL
-	if info := ExamineURI("http://internode.dl.sourceforge.net/project/yodl/yodl/3.05.01/yodl_3.05.01.orig.tar.gz"); info != nil {
-		if info.PkgName != "yodl" {
-			t.FailNow()
+	uri_tests["http://internode.dl.sourceforge.net/project/yodl/yodl/3.05.01/yodl_3.05.01.orig.tar.gz"] = SourceInfo{PkgName: "yodl", Version: "3.05.01.orig"}
+
+	for uri, expected := range uri_tests {
+		computed := ExamineURI(uri)
+		if computed.PkgName != expected.PkgName {
+			t.Fatalf("PkgName: Expected: %v, got: %v\n", expected.PkgName, computed.PkgName)
 		}
-		if info.Version != "3.05.01.orig" {
-			t.FailNow()
+		if computed.Version != expected.Version {
+			t.Fatalf("Version: Expected: %v, got: %v\n", expected.Version, computed.Version)
 		}
-	} else {
-		t.FailNow()
 	}
 }
 
