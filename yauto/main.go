d3c8656cfc61bfa1fd190e35c932e100b26797d9
Ikey Doherty <ikey@solus-project.com>
2016-10-30 08:36:18 +0000

    yauto: Begin defining the usage pattern
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- yauto/main.go
+++ yauto/main.go
@@ -19,9 +19,19 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprintf(os.Stderr, "Not yet implemented\n")
+
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s [archive URIs]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	archives := os.Args[1:]
+	fmt.Fprintf(os.Stderr, "Fetching: %s\n", strings.Join(archives, ", "))
+
 	os.Exit(1)
 }
